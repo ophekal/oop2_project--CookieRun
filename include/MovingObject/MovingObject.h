769614aa91fddbe443b84f8b8410c5cc6abc1dac
Livnat Arama <livnatar@std.hac.ac.il>
2024-06-26 16:10:34 +0300

    4:10

--- include/MovingObject/MovingObject.h
+++ include/MovingObject/MovingObject.h
@@ -2,8 +2,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "GameObject.h"
-//#include "StaticObject/StaticObject.h"
-//#include "Animation.h"
 #include "Macros.h"
 
 
@@ -13,19 +11,8 @@ public:
 	MovingObject(const sf::Sprite& sprite, float speed, const sf::Vector2f& position);
 	virtual ~MovingObject() = default;
 	virtual void setPosition(float x, float y);
-	//virtual void updateAnimation(sf::Time deltaTime) {};
-	//virtual void movement(sf::Time deltaTime, const sf::RectangleShape& board,
-	//	const std::unique_ptr<MovingObjects>& mouse,
-	//	const std::vector<std::unique_ptr<StaticObjects>>& staticObjects) = 0;
-	//void move(sf::Time deltaTime);
-	//void setDirection(const sf::Vector2f& direction);
-	//bool isMovementValid(const sf::RectangleShape& board, const sf::RectangleShape& newPosition)const;
-	//bool positionChange()const;
 	sf::Vector2f getPosition()const;
 	float getSpeed()const;
-	//sf::Vector2f getInitPosition()const;
-
-	//float distance(const sf::Vector2f& v1, const sf::Vector2f& v2);
 	void setObjectSpeed(float speed);
 
 
--- src/Collisions/HandleCollision.cpp
+++ src/Collisions/HandleCollision.cpp
@@ -36,17 +36,13 @@ void HandleCollision::initializeCollisionMap()
     m_collisionMap[Key(typeid(Player), typeid(StaticObstcale))] = &playerObstcale;
     m_collisionMap[Key(typeid(Player), typeid(Floor))] = &playerFloor;
     m_collisionMap[Key(typeid(Player), typeid(Enhance))] = &playerEnhance;
-    //m_collisionMap[Key(typeid(Player), typeid(Dynamite))] = &playerDynamite;
     m_collisionMap[Key(typeid(Player), typeid(Weapon))] = &playerWeapon;
     m_collisionMap[Key(typeid(Player), typeid(Boost))] = &playerBoost;
     //m_collisionMap[Key(typeid(Player), typeid(Magnet))] = &playerMagnet;
     m_collisionMap[Key(typeid(Player), typeid(FlyingGift))] = &playerFlyingGift;
-    //m_collisionMap[Key(typeid(Player), typeid(CarrotMissile))] = &playerCarrotMissile;
     m_collisionMap[Key(typeid(Player), typeid(Enemy))] = &playerEnemy;
     //m_collisionMap[Key(typeid(Enemy), typeid(Gift))] = &enemyGift;
    // m_collisionMap[Key(typeid(Enemy), typeid(Player))] = &enemyPlayer;
-   // m_collisionMap[Key(typeid(Enemy), typeid(Dynamite))] = &enemyDynamite;
-   // m_collisionMap[Key(typeid(Enemy), typeid(CarrotMissile))] = &enemyCarrotMissile;
 
     // NEED TO ADDFUNCTION THAT HANDLES ENEMY GETTING HIT BY MISSILE OF DYNAMITE EXPLODING?
 }
