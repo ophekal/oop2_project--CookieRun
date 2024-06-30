c0419e015348ced938cb3bf4e38c52573e5e046d
Livnat Arama <livnatar@std.hac.ac.il>
2024-06-30 10:45:39 +0300

    10:45

--- include/Loader.h
+++ include/Loader.h
@@ -12,15 +12,15 @@ class Loader
 {
 public:
     Loader() = default;
-    //void updateMembers(LevelCommand& level);
+    void updateMembers(LevelCommand& level);
 
-    void updateMembers(int levelNumber, std::vector < std::unique_ptr<AnimationObject>>& animationObjects,
-        std::vector < std::unique_ptr<StaticObject>>& staticObjects, std::vector<std::unique_ptr<Enemy>>& enemies,
-        std::vector< std::unique_ptr<Coin>>& coins, sf::Vector2f& flagPosition);
+   // void updateMembers(int levelNumber, std::vector < std::unique_ptr<AnimationObject>>& animationObjects,
+       // std::vector < std::unique_ptr<StaticObject>>& staticObjects, std::vector<std::unique_ptr<Enemy>>& enemies,
+       // std::vector< std::unique_ptr<Coin>>& coins, sf::Vector2f& flagPosition);
     
 private:    
-    //void addObjectsToVectors(LevelCommand& currLevel, const level& levelPart);
-    //void createRandomEnemy(LevelCommand& currLevel, float startX, float endX);
+    void addObjectsToVectors(LevelCommand& currLevel, const level& levelPart);
+    void createRandomEnemy(LevelCommand& currLevel, float startX, float endX);
     sf::Vector2f m_lastObjectPosition = { 0,0 };
 
 
